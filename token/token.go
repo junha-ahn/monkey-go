dc38b99312860a087e3c97897c9184f8fe70d375
Junha <junha.ahn.dev@gmail.com>
2023-02-18 10:52:18 +0000

    add: ch1. complete

--- token/token.go
+++ token/token.go
@@ -15,11 +15,19 @@ const (
 	INT = "INT"
 
 	ASSIGN = "="
+	EQ = "=="
+	NOT_EQ = "!="
 	PLUS = "+"
+	MINUS = "-"
+	BANG = "!"
+	ASTERISK = "*"
+	SLASH = "/"
 
 	COMMA = ","
 	SEMICOLON = ";"
 
+	LT = "<"
+	GT = ">"
 	LPAREN = "("
 	RPAREN = ")"
 	LBRACE = "{"
@@ -27,4 +35,26 @@ const (
 
 	FUNCTION = "FUNCTION"
 	LET = "LET"
+	TRUE = "TRUE"
+	FALSE = "FALSE"
+	IF = "IF"
+	ELSE = "ELSE"
+	RETURN = "RETURN"
 )
+
+var keywords = map[string]TokenType {
+	"fn": FUNCTION,
+	"let": LET,
+	"true": TRUE,
+	"false": FALSE,
+	"if": IF,
+	"else": ELSE,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
